96391c8d992cad4732a8b9267e6a460e37180adf
MrMoop007 <XanderMoore@proton.me>
2025-08-02 16:11:20 +0100

    Update menu.py
    Introduced a new menu architecture, better event based handling for the sliders and expanded the functionality of the options sub-menu.

--- menu.py
+++ menu.py
@@ -14,6 +14,7 @@ class button:
         self.text = text
         self.screen = screen
         self.function = function
+        self.variable = None #to differentiate the button from a slider
 
     #detects if the player's mouse is over the button
     #for things like changing colour and detecting clicks
@@ -36,7 +37,10 @@ class button:
         return self.function
 
 class slider(button):
-    def __init__(self, xpos, ypos, width, height, colour, screen, function):
+    #variable is the variable this slider is responsible for
+    #it should be a string to act as a key for the dictionary which stores
+    #the user's settings
+    def __init__(self, xpos, ypos, width, height, colour, screen, function, variable):
         super().__init__(xpos, ypos, width, height, colour, "", screen, function) #no text on the slider and the function is handled differently
         self.xpos = xpos
         self.ypos = ypos
@@ -45,20 +49,22 @@ class slider(button):
         self.colour = colour
         self.screen = screen
         self.function = function
-        self.original_function = function
+        self.value = 50
+        self.variable = variable
 
     def draw_self(self):
         pygame.draw.rect(self.screen, self.colour, (self.xpos, self.ypos, self.width, self.height))
-        self.screen.blit(text, rectangle)
+        #draws a second rectangle of the opposite colour to show the slider's value
+        pygame.draw.rect(self.screen, (255 - self.colour[0], 255-self.colour[1], 255-self.colour[2]), (self.xpos, self.ypos, self.width*self.value/100, self.height))
 
     def detect_mouse(self, mouse_position):
         if self.xpos+self.width >= mouse_position[0] >= self.xpos and self.ypos+self.height >= mouse_position[1] >= self.ypos:
-            self.function = self.original_function + str((mouse_position[0]-self.xpos)*100/self.width)
+            self.value = (mouse_position[0]-self.xpos)*100/self.width
             return True
         else:
             return False
 
-    
+
 
 def initalise_menu_screen(): #returns a list of objects that get rendered
     return [button(260, 600, 200, 76, (0, 0, 0), "Exit", screen, "quit"), button(260, 500, 200, 76, (0, 0, 0), "Options", screen, "open options"),
@@ -69,4 +75,5 @@ def initalise_menu_screen(): #returns a list of objects that get rendered
 def open_options_menu(): #creates a surface and then places the options menus buttons on it
     return[button(260, 600, 200, 76, (0, 0, 0), "Close", screen, "close options"),
     button(210, 500, 300, 76, (0, 0, 0), "Toggle Fullscreen", screen, "toggle fullscreen"),
-    slider(210, 400, 300, 76, (255, 255, 255), screen, "set volume")]
+    slider(210, 300, 300, 76, (255, 255, 255), screen, "set volume", "volume"),
+    slider(210, 400, 300, 76, (255, 255, 255), screen, "set simulation speed", "simulation speed")]
