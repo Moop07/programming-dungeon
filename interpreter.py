d005384af53730d926282168824f34585621b573
MrMoop007 <XanderMoore@proton.me>
2025-08-09 22:31:46 +0100

    Update interpreter.py
    Interpreter can now handle arbitrary length expressions in the form INTEGER, OPERATOR, INTEGER, OPERATOR, etc.
    
    improved error handling and fixed some bugs

--- interpreter.py
+++ interpreter.py
@@ -1,17 +1,17 @@
 import re
 
 class token:
-    def __init__(self, type, value = None):
+    def __init__(self, type, value = 0):
         self.type = type
         self.value = value
 
 class interpreter:
     def __init__(self, text):
         self.text = re.findall(r'\w+|[^\w\s]', text)
-        print(self.text)
         self.token_list = []
+        self.token_index = 0
         self.current_token = None
-    
+
     def tokeniser(self):
         #token_string refers to the token while it is a string, also known as a lexeme
         for token_string in self.text:
@@ -19,34 +19,56 @@ class interpreter:
                 self.token_list.append(token("INTEGER", int(token_string)))
             elif token_string == "+":
                 self.token_list.append(token("PLUS"))
+            elif token_string == "-":
+                self.token_list.append(token("MINUS"))
+            elif token_string == "/":
+                self.token_list.append(token("DIVIDE"))
+            elif token_string == "*":
+                self.token_list.append(token("MULTIPLY"))
             else:
                 raise ValueError(f'''Could not parse token "{token_string}"''')
-|
+
+        self.token_list.append(token("EOF"))
+
     def get_next_token(self):
-        del self.token_list[0]
-        return self.token_list[0]
+        self.token_index += 1
+        self.current_token = self.token_list[self.token_index]
 
     def evaluate_expression(self):
-        #for now this can only handle INT + INT expressions#
         self.tokeniser()
-        self.current_token =self.token_list[0]
+        self.current_token = self.token_list[0]
+        value = 0
 
-        #get the left number and move to the next token
-        left = self.current_token
-        self.current_token = self.get_next_token()
+        while self.current_token.type != "EOF":
+            #if the token is an operator we apply that operation to the running total
+            #if the token is an integer it is added to the total
+            #this is because the first integer should be the only one the interpreter doesn't indirectly skip
+            match self.current_token.type:
+                case "MINUS":
+                    self.token_index += 1
+                    self.current_token = self.token_list[self.token_index]
+                    value -= self.current_token.value
+                case "PLUS":
+                    self.token_index += 1
+                    self.current_token = self.token_list[self.token_index]
+                    value += self.current_token.value
+                case "INTEGER":
+                    value += self.current_token.value
+                case "DIVIDE":
+                    self.token_index += 1
+                    self.current_token = self.token_list[self.token_index]
+                    value /= self.current_token.value
+                case "MULTIPLY":
+                    self.token_index += 1
+                    self.current_token = self.token_list[self.token_index]
+                    value *= self.current_token.value
+            if self.current_token.type != "EOF":
+                self.token_index += 1
+                self.current_token = self.token_list[self.token_index]
 
-        #get the operator and move to the next token
-        operator = self.current_token
-        self.current_token = self.get_next_token()
-
-        #finally, get the right number and last token
-        right = self.current_token
-
-        if operator.type == "PLUS":
-            result = left.value + right.value
-        return result
+        return value
 
 
-my_interpreter = interpreter("2 - 2")
+my_interpreter = interpreter(input("calc> "))
 
 print(my_interpreter.evaluate_expression())
