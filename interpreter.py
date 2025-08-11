e04af885111db76c3f585555faf780781b137f27
MrMoop007 <XanderMoore@proton.me>
2025-08-11 17:10:57 +0100

    Update interpreter.py
    improved evaluate_expression method to handle order of operations and brackets
    
    added interpret method 
    
    interpret method can define a variable using the syntax "let (variable name) = (expression)"

--- interpreter.py
+++ interpreter.py
@@ -1,7 +1,7 @@
 import re
 
 class token:
-    def __init__(self, type, value = 0):
+    def __init__(self, type, value = None):
         self.type = type
         self.value = value
 
@@ -18,15 +18,25 @@ class interpreter:
             if token_string.isdigit(): #if the token is a number
                 self.token_list.append(token("INTEGER", int(token_string)))
             elif token_string == "+":
-                self.token_list.append(token("PLUS"))
+                self.token_list.append(token("PLUS", "+"))
             elif token_string == "-":
-                self.token_list.append(token("MINUS"))
+                self.token_list.append(token("MINUS", "-"))
             elif token_string == "/":
-                self.token_list.append(token("DIVIDE"))
+                self.token_list.append(token("DIVIDE", "/"))
             elif token_string == "*":
-                self.token_list.append(token("MULTIPLY"))
+                self.token_list.append(token("MULTIPLY", "*"))
+            elif token_string == "(":
+                self.token_list.append(token("OPEN BRACKET", "("))
+            elif token_string == ")":
+                self.token_list.append(token("CLOSE BRACKET", ")"))
+            elif token_string == ";":
+                self.token_list.append(token("SEMICOLON", ";"))
+            elif token_string == "let":
+                self.token_list.append(token("DEFINE VARIABLE", "let"))
+            elif token_string == "=":
+                self.token_list.append(token("ASSIGN", "="))
             else:
-                raise ValueError(f'''Could not parse token "{token_string}"''')
+                self.token_list.append(token("UNDEFINED", token_string))
 
         self.token_list.append(token("EOF"))
 
@@ -35,40 +45,45 @@ class interpreter:
         self.current_token = self.token_list[self.token_index]
 
     def evaluate_expression(self):
+        expression = "" #to store our expression in
+        while self.current_token.type not in ["EOF", "SEMICOLON"]: #if it reaches the end it should stop
+            #add on the value of the current token
+            expression += f"{self.current_token.value} "
+            #advance to the next token
+            self.get_next_token()
+
+        return eval(expression)
+
+    def interpret(self):
         self.tokeniser()
-        self.current_token = self.token_list[0]
-        value = 0
+        self.current_token = self.token_list[self.token_index]
 
         while self.current_token.type != "EOF":
-            #if the token is an operator we apply that operation to the running total
-            #if the token is an integer it is added to the total
-            #this is because the first integer should be the only one the interpreter doesn't indirectly skip
-            match self.current_token.type:
-                case "MINUS":
-                    self.token_index += 1
-                    self.current_token = self.token_list[self.token_index]
-                    value -= self.current_token.value
-                case "PLUS":
-                    self.token_index += 1
-                    self.current_token = self.token_list[self.token_index]
-                    value += self.current_token.value
-                case "INTEGER":
-                    value += self.current_token.value
-                case "DIVIDE":
-                    self.token_index += 1
-                    self.current_token = self.token_list[self.token_index]
-                    value /= self.current_token.value
-                case "MULTIPLY":
-                    self.token_index += 1
-                    self.current_token = self.token_list[self.token_index]
-                    value *= self.current_token.value
-            if self.current_token.type != "EOF":
-                self.token_index += 1
-                self.current_token = self.token_list[self.token_index]
-
-        return value
+            #handles defining variables
+            if self.current_token.type == "DEFINE VARIABLE":
+                self.get_next_token()
+                #token type should be undefined until it's defined as a variable
+                if self.current_token.type == "UNDEFINED":
+                    variable_name = self.current_token.value
+                else:
+                    raise Exception("Variable name cannot be identical to another token")
+                self.get_next_token()
+                #the next token must be "="
+                if self.current_token.type != "ASSIGN":
+                    raise Exception("Expected '=' to follow variable name")
+                self.get_next_token()
+                #we evaluate the expression on the other side of the equals sign and assign that as our value
+                variable_value = self.evaluate_expression()
+                #the next token must be ";"
+                if self.current_token.type != "SEMICOLON":
+                    raise Exception("Expected ';' to follow variable value")
+                
+                #temporary until variables are fully implemented
+                print(f"defining variable {variable_name} with a stored value of {variable_value}")
+            self.get_next_token()
 
 
-my_interpreter = interpreter(input("calc> "))
 
-print(my_interpreter.evaluate_expression())
+my_interpreter = interpreter(input("> "))
+
+my_interpreter.interpret()
