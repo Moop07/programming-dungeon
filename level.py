a28acbceb7f6339d439186e7086209cf3d1839e5
Moop07 <XanderMoore@proton.me>
2025-09-02 21:45:20 +0100

    Update level.py

--- level.py
+++ level.py
@@ -1,4 +1,5 @@
 import pygame
+import interpreter
 
 screen = None
 
@@ -11,6 +12,29 @@ class player:
         self.xpos = xpos
         self.ypos = ypos
         self.image = pygame.image.load("main_character.png")
+        self.interpreter = None
 
     def draw_self(self):
         screen.blit(self.image, (self.xpos, self.ypos))
+    
+    def follow_instructions(self, code):
+        self.interpreter = interpreter.interpreter(code)
+        self.events = self.interpreter.interpret()
+        print(self.events)
+    
+    def get_code(self, code):
+        self.code = code
+        self.follow_instructions(self.code)
+
+player = player(500, 500)
+player.get_code('''let a = 0;
+while (a < 10){
+    if (a < 5 ){
+        move_player_up();
+    }
+    if (a > 5){
+        move_player_down();
+    }
+    a = a + 1;
+    }
+''')
